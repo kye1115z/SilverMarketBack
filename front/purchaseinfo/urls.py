cb09c696154d91420265dd3d2bd163d2c0d3e837
nayeong lee <lny021102@gmail.com>
2023-09-03 15:36:38 +0900

    delete front

--- front/purchaseinfo/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.ListPurchaseinfo.as_view()),
-    path('<int:pk>/', views.DetailPurchaseinfo.as_view()),
-]
